a1b5cccc4e429f6c6c98987982904319f4064913
leonel-ai <leonel-ai@users.noreply.github.com>
2016-11-12 22:42:57 -0600

    Update mqtt-subscriber.py

--- module/mqtt-subscriber.py
+++ module/mqtt-subscriber.py
@@ -1,6 +1,6 @@
 # Spot Security: Module - Rasp Pi - AWS IoT
 #!/usr/bin/python3
-#initiate cmd: python3 iot-mqtt-subscriber.py          
+#initiate cmd: python3 mqtt-subscriber.py          
 
 #required libraries
 import sys                                 
