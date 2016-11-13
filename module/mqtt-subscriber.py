a69b5edc24deceb6cd5cc9202a1ee36ea68a2f9e
leonel-ai <leonel-ai@users.noreply.github.com>
2016-11-12 22:03:14 -0600

    Update and rename device/mqtt-subscriber.py to module/mqtt-subscriber.py

--- module/mqtt-subscriber.py
+++ module/mqtt-subscriber.py
@@ -29,17 +29,17 @@ mqttc.on_message = on_message
 
 # Configure network encryption and authentication options. Enables SSL/TLS support.
 # Add client-side certificates and enabling tlsv1.2 support as req'd by aws-iot service
-mqttc.tls_set("/home/username/root-CA.crt",
-	            certfile="/home/username/6fdda68178-certificate.pem.crt",
-	            keyfile="/home/username/6fdda68178-private.pem.key",
+mqttc.tls_set("/home/auth_data/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem.crt",
+	            certfile="home/auth_data/mod-7ceea45e66-certificate.pem.crt",
+	            keyfile="home/auth_data/mod-7ceea45e66-private.pem.key",
               tls_version=ssl.PROTOCOL_TLSv1_2, 
               ciphers=None)
 
 # Connect to aws-account-specific-iot-endpoint
-mqttc.connect("A2DL8ZE59089FKF.iot.us-west-2.amazonaws.com", port=8883) #AWS IoT service hostname and portno
+mqttc.connect("a27123o8bvt7qk.iot.us-west-2.amazonaws.com/things/iot-module/shadow", port=8883) #AWS IoT service hostname and portno
 
 # Topic to publish to
-mqttc.subscribe("$aws/things/mqtt-listener/shadow/update/#", qos=1) #The names of these topics start with $aws/things/thingName/shadow."
+mqttc.subscribe("$aws/things/iot-module/shadow/update", qos=1) #The names of these topics start with $aws/things/thingName/shadow."
 
 # Auto handles reconnecting
 mqttc.loop_forever()
