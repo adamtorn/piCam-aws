53823a2123c33b9ef3ab75fa8dbe3e03e6626e90
leonel-ai <leonel-ai@users.noreply.github.com>
2016-11-12 19:43:20 -0600

    Create mqtt-client.py

---
+++ mqtt-client.py
@@ -0,0 +1,12 @@
+# Import SDK packages
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
+
+# For certificate based connection
+myMQTTClient = AWSIoTMQTTClient("myClientID")
+
+# Basic MQTT operations
+myMQTTClient.connect()
+myMQTTClient.publish("myTopic", "myPayload", 0)
+myMQTTClient.subscribe("myTopic", 1, customCallback)
+myMQTTClient.unsubscribe("myTopic")
+myMQTTClient.disconnect()
